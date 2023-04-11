cadd2e2a5d8fec1ff501291166c268bc11cc1b34
Riley <riley.childs@yahoo.com>
2023-04-11 13:01:52 -0400

    Created user pick and house pick

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+let userChoice
+let houseChoice
+
 const displayRules = document.getElementsByClassName("rules-btn")[0];
 const rulesModal = document.getElementsByClassName("rules-modal")[0];
 const closeRulesBtn = document.getElementsByClassName(
   "rules-modal__container--close"
 )[0];
+
+const options = document.getElementsByClassName("options")[0]
+const choices = document.getElementsByClassName("choice-box")
+
+const pick = document.getElementsByClassName("pick")[0]
+const yourPick = document.getElementsByClassName("pick__your-pic--container")[0]
+const housePick = document.getElementsByClassName("pick__house-pic--container")[0]
+
+function getNo(max) {
+  return Math.floor(Math.random() * max);
+}
 
 displayRules.onclick = () => {
   if (rulesModal.style.display === "none" || rulesModal.style.display === "") {
@@ -19,3 +33,16 @@ window.onclick = (event) => {
     rulesModal.style.display = "none";
   }
 };
+
+Object.keys(choices).forEach((choice) => {
+  choices[choice].onclick = () => {
+    options.style.display = "none"
+    pick.style.display ="flex"
+
+    userChoice = choices[choice]
+    houseChoice = choices[getNo(3)]
+
+    yourPick.appendChild(userChoice)
+    housePick.appendChild(houseChoice)
+  }
+})
